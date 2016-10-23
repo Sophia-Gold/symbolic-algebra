89f7a5fa710ca3e4664da37a9952493a7b14bf16
Sophia Gold <sophiagoldnyc@gmail.com>
2016-10-23 12:01:41 -0400

    added project.clj description

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject symbolic-algebra "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "symbolic algebra library with numerical tower"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
